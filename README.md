a44568fc33c4779126846dae3090df42d6c9eca4
unknown <iktasolork22@gmail.com>
2022-10-18 14:24:15 -0600

    added js, html files and updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cpnt262-achievements-1-numbers
+- Author: Rustu Mesut Eser
+- Live site: https://silentchallenger.github.io/cpnt262-achievements-1-numbers
---
+++ js/client.js
@@ -0,0 +1,9 @@
+const number = 7
+console.log("Before -", number);
+
+console.log("After - Conver to string", number.toString());
+console.log("After - Is it NaN", Number.isNaN(number));
+console.log("After - Is if finite", Number.isFinite(number));
+console.log("After - Check is it integer", Number.isInteger(number));
+console.log("After - Convert to integer ", Number.parseInt(number));
+console.log("After - Conver to float", Number.parseFloat(number));
